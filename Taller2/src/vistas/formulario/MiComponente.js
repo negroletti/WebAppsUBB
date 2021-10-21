00a33fa963a5afcdd554f65517dcceaac9c7867b
negroletti <negroletti@gmail.com>
2021-10-21 18:40:50 -0300

    Delete

--- Taller2/src/vistas/formulario/MiComponente.js
+++ Taller2/src/vistas/formulario/MiComponente.js
@@ -1,12 +1,18 @@
+
+import {useMediaQuery} from 'react-responsive';
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MaterialDatatable from "material-datatable";
+import {Container, Grid, Button, Typography, TextField} from '@material-ui/core'
+import Swal from 'sweetalert2';
 
 const MiComponente = () => {
     const [nombre, setNombre] = useState("");
     const [apellido, setApellido] = useState("");
     const [personas, setPersonas] = useState([]);
     const [idModificar, setIdModificar] = useState("");
+    const [accion, SetAccion] = useState("Guardar");
+    const isMobile = useMediaQuery({query: `(max-width: 760px)`});
 
     const handleInputChangeNombre = (event) => {
         setNombre(event.target.value);
@@ -15,6 +21,7 @@ const MiComponente = () => {
     const handleInputChangeApellido = (event) => {
         setApellido(event.target.value);
     };
+
 
     const enviarDatos = () => {
         console.log(`Enviando datos nombre:${nombre} y apellido:${apellido}`);
@@ -39,7 +46,7 @@ const MiComponente = () => {
         }
     }
 
-    function guardarPersona() {
+    const guardarPersona = () => {
         axios
             .post("http://192.99.144.232:5000/api/personas", {
                 nombre: nombre,
@@ -77,6 +84,38 @@ const MiComponente = () => {
                 console.log(error);
             });
     };
+    const borrarPersona = () => {
+        Swal.fire({
+            title: '¿Quieres borrar a este usuario?',
+            showDenyButton: true,
+            confirmButtonText: 'Borrar',
+            denyButtonText: `No Borrar`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              axios
+                .delete(`http://192.99.144.232:5000/api/personas/${idModificar}`, {
+                nombre: nombre,
+                apellido: apellido,
+            })
+            .then(function (response) {
+                if (response.status === 200) {
+                    Swal.fire('Usuario eliminado!', '', 'success')
+                    setIdModificar("");
+                    getPersonas();
+                } else {
+                    Swal.fire('Error al borrar!', '', 'info')
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+            } else if (result.isDenied) {
+              Swal.fire('El usuario no fue borrado', '', 'info')
+            }
+          })
+        
+    };
     const columns = [
         {
             name: "Nombre",
@@ -109,39 +148,40 @@ const MiComponente = () => {
     };
 
     return (
-        <>
-            <h1>Formulario</h1>
-            <div>
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Nombre"
-                        name="nombre"
-                        onChange={handleInputChangeNombre}
-                        value={nombre}
-                    ></input>
-                </div>
+        <Container maxWidth="md">
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={12}>
+                    <Typography variant="h6">
+                        Personas
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={6} fullWidth>
+                    <TextField id="nombre" label="Nombre" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <TextField id="apellido" label="Apellido" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item xs={12} md={2}>
+                    <Button variant="contained" onclick={idModificar ? editarPersona : enviarDatos} color="primary"  
+                    fullWidth>{accion}</Button>
+                </Grid>
+                <Grid item xs={12} md={2}>
+                    <Button variant="contained" disabled={idModificar ? false : true} onClick={borrarPersona} color="secondary" fullWidth>Eliminar</Button>
+                </Grid>
+            </Grid>
 
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Apellido"
-                        name="apellido"
-                        onChange={handleInputChangeApellido}
-                        value={apellido}
-                    ></input>
-                </div>
-                <button onClick={idModificar ? editarPersona : enviarDatos}>
-                    Enviar
-                </button>
-            </div>
+      
+            <Grid item xs={12} md={12} className="tabla">
             <MaterialDatatable
-                title={"Lista de trabajadores"}
+                title={"Employee List"}
                 data={personas}
                 columns={columns}
                 options={options}
             />
-        </>
+            
+        </Grid>
+      
+        </Container>
     );
 };
 export default MiComponente;
