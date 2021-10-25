3f110d2c70e91233465281b1bfcdf8968906e3db
SoimSei <imsergio@hotmail.es>
2021-10-25 18:34:37 -0300

    a

--- Taller2/src/App.js
+++ Taller2/src/App.js
@@ -2,13 +2,18 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import MiComponente from "./vistas/formulario/MiComponente";
 import Componente2 from "./vistas/formulario/Componente2";
+import { NavLink } from "react-router-dom";
 
 function App() {
     function Navbar() {
         return (
             <nav className="topnav">
-                <Link to="/otro">Integrantes</Link>
-                <Link to="/personas">Personas</Link>
+                <NavLink to="/otro" activeClassName="active">
+                    Integrantes
+                </NavLink>
+                <NavLink to="/personas" activeClassName="active">
+                    Personas
+                </NavLink>
             </nav>
         );
     }
