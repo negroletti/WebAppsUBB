8ab5f655a9b8d13e2ee7e850db543cb4120d44e7
negroletti <negroletti@gmail.com>
2021-11-09 23:30:19 -0300

    Libro

---
+++ backend_2021_02-main/modelos/libro.js
@@ -0,0 +1,23 @@
+'use strict'
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+libroSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El titulo es necesario']
+    },
+    autor: {
+        type: String,
+        required: [true, 'El autor es necesario']
+    },
+    anio: {
+        type: Number,
+        required: [true, 'El año es necesario']
+    },
+    idioma: {
+        type: String,
+        required: [true, 'El idioma es necesario'],
+        enum: ['ESP', 'ING']
+    },
+})
+module.exports = mongoose.model('libro', libroSchema)
---
+++ backend_2021_02-main/routes/libroRoute.js
@@ -0,0 +1,14 @@
+'use strict'
+
+var express = require('express');
+var libroController = ('../controllers/libroController');
+
+var api = express.Router();
+
+api.post('/libros', libroController.guardar);
+api.get('/libros', libroController.listar);
+api.get('/libros/:id', libroController.listarUno);
+api.put('/libros/:id', libroController.modificar);
+api.delete('/libros/:id', libroController.eliminar);
+
+module.exports = api;
